52881a8dae181c3e0b6085015c4d533799127463
Ludovic Pouey <gogopex@gmail.com>
2020-06-28 14:19:12 +0200

    battling annotations

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController 
+{
+    public function number(): Response
+    {
+        $number = random_int(0,100);
+
+        return new Response('<html><body>Lucky number: '.$number.'</body></html>');
+    }
+}
